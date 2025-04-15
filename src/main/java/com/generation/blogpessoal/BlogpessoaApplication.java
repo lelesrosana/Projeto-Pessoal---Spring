a5443ee2e3b456fc9e23e4796abf00fcde55087d
Rosana Leles <97849886+lelesrosana@users.noreply.github.com>
2025-04-15 01:32:19 -0300

    Adicionando projeto

---
+++ src/main/java/com/generation/blogpessoal/BlogpessoaApplication.java
@@ -0,0 +1,13 @@
+package com.generation.blogpessoal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogpessoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogpessoaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/blogpessoal/BlogpessoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.blogpessoal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogpessoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
